09b035d09092bcb81dc236fecc81f40541537d20
Jay Viloria <jviloria96744@gmail.com>
2020-09-13 13:22:26 -0700

    Revert "Add test failure to workflow"
    This reverts commit 7313ecd837b122c4a407ce38aeb2d3e5c8c76ce8.

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,15 +1,9 @@
-import React from "react";
-import { render } from "@testing-library/react";
-import App from "./App";
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from './App';
 
-test("renders learn react link", () => {
+test('renders learn react link', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
-
-test("renders fail link", () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/I am a negative test/i);
   expect(linkElement).toBeInTheDocument();
 });

0faffbfd8987af0f969aea8e09a0774c902764ab
Jay Viloria <jviloria96744@gmail.com>
2020-09-14 14:36:51 -0700

    Add test failure to test revert step in workflow

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -7,3 +7,9 @@ test("renders learn react link", () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test("fail test", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/I should fail/i);
+  expect(linkElement).toBeInTheDocument();
+});

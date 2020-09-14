b5bed2703928bcd818bde92e669e1a1570f656fa
Jay Viloria <jviloria96744@gmail.com>
2020-09-14 09:21:39 -0700

    Adding fail test to test workflow logic

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -7,3 +7,9 @@ test("renders learn react link", () => {
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test("failure test", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/I will fail/i);
+  expect(linkElement).toBeInTheDocument();
+});
